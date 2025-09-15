48c305ae84953c02f5b9b10ac38b4d45b6e2b44c
Carissa K <carissa.cks@gmail.com>
2025-09-16 02:32:47 +0700

    chore: third task, optimize reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -9,17 +9,33 @@ export class ReportsController {
   report() {
     return {
       'accounts.csv': this.reportsService.state('accounts'),
+      'accountsAsync.csv': this.reportsService.state('accountsAsync'),
+      'accountsModified.csv': this.reportsService.state('accountsModified'),
       'yearly.csv': this.reportsService.state('yearly'),
+      'yearlyModified.csv': this.reportsService.state('yearlyModified'),
       'fs.csv': this.reportsService.state('fs'),
+      'fsModified.csv': this.reportsService.state('fsModified'),
+      getFilesDetail: this.reportsService.state('getFilesDetail'),
     };
   }
 
   @Post()
-  @HttpCode(201)
+  // Returning 202 to indicate the request
+  // has been accepted but still processing
+  @HttpCode(202)
   generate() {
-    this.reportsService.accounts();
-    this.reportsService.yearly();
-    this.reportsService.fs();
-    return { message: 'finished' };
+    // Run all report generation tasks in the background
+    setImmediate(() => {
+      this.reportsService.getCsvFilesDetail();
+      this.reportsService.accountsModified();
+      this.reportsService.accountsAsync();
+      this.reportsService.accounts();
+      this.reportsService.yearly();
+      this.reportsService.yearlyModified();
+      this.reportsService.fs();
+      this.reportsService.fsModified();
+    });
+
+    return { message: 'generating reports' };
   }
 }
